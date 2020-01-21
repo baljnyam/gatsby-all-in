54b04be20b37fb1e69983134ce95c1517d0ff0c6
baljnyam <baljnyam@gmail.com>
2020-01-21 15:41:49 +1100

    nested layout

---
+++ tutorial-part-three/src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/layout"
+export default () => (
+  <Layout>
+    <h1>About me</h1>
+    <p>What i am is what think i am. I the man who is chasing his passion</p>
+  </Layout>
+)
